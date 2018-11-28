93ce37ff3b240d2282da2b003d6b3536107a739b
Carlos Escriche <escrichov@gmail.com>
2018-11-28 19:27:44 +0100

    Modified API url list

--- shortener_app/urls.py
+++ shortener_app/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
 
     path('api/url/create', views.api_url_create, name='api_delete_create'),
     path('api/url/delete/<slug:uid>', views.api_url_delete, name='api_delete_url'),
-    path('api/urls', views.api_url_list, name='api_list_urls'),
+    path('api/url/list', views.api_url_list, name='api_list_urls'),
 ]
