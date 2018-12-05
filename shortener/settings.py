dc68c2292c071e9704882141fff59ea2676cf34e
Carlos Escriche <escrichov@gmail.com>
2018-12-06 00:26:53 +0100

    Added allowed hosts

--- shortener/settings.py
+++ shortener/settings.py
@@ -145,7 +145,7 @@ class Development(Common):
     """
     DEBUG = True
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = ['carlos-shortener.herokuapp.com', 'cour.fun']
 
     INTERNAL_IPS = [
         '127.0.0.1'
