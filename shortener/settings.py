15bc781d4a4b2b1bc8ab1c716b7b0b4cc4e1c39a
Carlos Escriche <escrichov@gmail.com>
2018-12-06 00:10:59 +0100

    Added logging

--- shortener/settings.py
+++ shortener/settings.py
@@ -183,6 +183,38 @@ class Staging(Common):
 
     HOSTNAME = 'cour.fun'
 
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                        'pathname=%(pathname)s lineno=%(lineno)s ' +
+                        'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'testlogger': {
+                'handlers': ['console'],
+                'level': 'INFO',
+            }
+        }
+    }
 
 class Production(Staging):
     """
