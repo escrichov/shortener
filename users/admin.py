51859a134a85433ea883a1582ebde276e14cc93c
Carlos Escriche <escrichov@gmail.com>
2018-12-15 17:50:33 +0100

    Fixed problem with username field

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from users.forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import User
 
 
 class CustomUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = User
-    list_display = ['email', ]
+    search_fields = ('email', 'first_name', 'last_name', 'email')
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
     ordering = ('email', )
 
 
