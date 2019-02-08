cf1b242cd4250f511ec926981e40c0263eca7fa2
Carlos Escriche <escrichov@gmail.com>
2019-02-08 18:22:40 +0100

    Fixed command update geolocation data

--- shortener_app/management/commands/update_logs_geolocation.py
+++ shortener_app/management/commands/update_logs_geolocation.py
@@ -1,8 +1,8 @@
+from datetime import datetime
+import requests
 from django.core.management.base import BaseCommand
 from django.conf import settings
-from datetime import datetime
 from shortener_app.models import ShortUrlLog
-import requests
 
 
 def get_geolocation_data(ip):
@@ -21,7 +21,6 @@ def get_geolocation_data(ip):
 class Command(BaseCommand):
 
     def handle(self, **options):
-        now = datetime.utcnow()
         logs = ShortUrlLog.objects.filter(
             country_code=None, latitude=None, longitude=None)
 
@@ -31,3 +30,4 @@ class Command(BaseCommand):
                 log.country_code = geolocation_data.get('country_code')
                 log.latitude = geolocation_data.get('latitude')
                 log.longitude = geolocation_data.get('longitude')
+                log.save()
