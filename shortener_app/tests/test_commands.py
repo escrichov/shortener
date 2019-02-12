077ac73b79bb53e265efed245adf97275ac413bd
Carlos Escriche <escrichov@gmail.com>
2019-02-12 23:46:09 +0100

    Testing command update logs

--- shortener_app/tests/test_commands.py
+++ shortener_app/tests/test_commands.py
@@ -1,3 +1,4 @@
+import requests_mock
 from datetime import datetime
 from django.core.management import call_command
 from django.test import TestCase
@@ -54,12 +55,19 @@ class UpdateLogsGeolocationCommandTestCase(TestCase):
 
         args = []
         opts = {}
-        call_command('update_logs_geolocation', *args, **opts)
+        response = {
+            'country_code': 'es',
+            'latitude': 1.234,
+            'longitude': 23.91
+        }
+        with requests_mock.Mocker() as m:
+            m.get('http://api.ipstack.com/{ip}'.format(ip=log.ip), json=response, status_code=200)
+            call_command('update_logs_geolocation', *args, **opts)
 
         updated_log = ShortUrlLog.objects.get(id=log.id)
-        self.assertEqual(updated_log.country_code, None)
-        self.assertEqual(updated_log.latitude, None)
-        self.assertEqual(updated_log.longitude, None)
+        self.assertEqual(updated_log.country_code, response['country_code'])
+        self.assertEqual(updated_log.latitude, response['latitude'])
+        self.assertEqual(updated_log.longitude, response['longitude'])
 
     def test_updated_failed(self):
         " Test my custom command."
@@ -70,9 +78,50 @@ class UpdateLogsGeolocationCommandTestCase(TestCase):
 
         args = []
         opts = {}
-        call_command('update_logs_geolocation', *args, **opts)
+        response = {
+            'country_code': 'es',
+            'latitude': 1.234,
+            'longitude': 23.91
+        }
+        with requests_mock.Mocker() as m:
+            m.get('http://api.ipstack.com/{ip}'.format(ip=log.ip), json=response, status_code=400)
+            call_command('update_logs_geolocation', *args, **opts)
 
         updated_log = ShortUrlLog.objects.get(id=log.id)
         self.assertEqual(updated_log.country_code, None)
         self.assertEqual(updated_log.latitude, None)
         self.assertEqual(updated_log.longitude, None)
+
+    def test_no_logs(self):
+        " Test my custom command."
+
+        args = []
+        opts = {}
+        call_command('update_logs_geolocation', *args, **opts)
+
+    def test_no_update_already_updated_logs(self):
+        " Test my custom command."
+
+        log = ShortUrlLog()
+        log.ip = '90.74.206.135'
+        log.country_code = 'es'
+        log.latitude = 12.2
+        log.longitude = 323.23
+        log.save()
+
+        args = []
+        opts = {}
+        response = {
+            'country_code': 'en',
+            'latitude': 1.234,
+            'longitude': 23.91
+        }
+        with requests_mock.Mocker() as m:
+            m.get('http://api.ipstack.com/{ip}'.format(ip=log.ip), json=response, status_code=200)
+            call_command('update_logs_geolocation', *args, **opts)
+        call_command('update_logs_geolocation', *args, **opts)
+
+        updated_log = ShortUrlLog.objects.get(id=log.id)
+        self.assertEqual(updated_log.country_code, 'es')
+        self.assertEqual(updated_log.latitude, 12.2)
+        self.assertEqual(updated_log.longitude, 323.23)
