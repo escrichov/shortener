72aeaff4835f0eb6d792078db01bda3b87da0c2e
Carlos Escriche <escrichov@gmail.com>
2019-02-21 01:05:50 +0100

    Custom login form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 # users/forms.py
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django.contrib.auth import authenticate
+from django.utils.translation import ugettext_lazy as _
+from django import forms
 from .models import User
 
 
@@ -15,3 +18,40 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('email',)
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    """
+    Override the default AuthenticationForm to force email-as-username behavior.
+    """
+    email = forms.EmailField(label=_("Email"), max_length=75)
+
+    error_messages = {
+        'invalid_login': _("Please enter a correct email and password."),
+        'inactive': _("This account is inactive."),
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+        super().__init__(request, *args, **kwargs)
+        del self.fields['username']
+        self.fields.keyOrder = ['email', 'password']
+
+    def clean(self):
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(self.request, username=email, password=password)
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
+
+    def get_invalid_login_error(self):
+        return forms.ValidationError(
+            self.error_messages['invalid_login'],
+            code='invalid_login',
+            params={'email': self.username_field.verbose_name},
+        )
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 # users/views.py
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth.views import LoginView
 
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+
+class Login(LoginView):
+    form_class = CustomAuthenticationForm
