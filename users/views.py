561a9bd5060bc157f53ec06eff5490065abc6978
Carlos Escriche <escrichov@gmail.com>
2019-02-21 01:24:30 +0100

    Added form errors

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import LoginView
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
 
 
 class Login(LoginView):
