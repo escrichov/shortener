7da1b615e1c8ce02db30cb9946f5f11fc5e1a520
Carlos Escriche <escrichov@gmail.com>
2019-03-21 19:22:08 +0100

    Serving static files from s3

--- shortener/settings.py
+++ shortener/settings.py
@@ -40,6 +40,7 @@ class Common(Configuration):
         'users',
         'django.contrib.admin',
         'rest_framework',
+        'storages',
     ]
 
     MIDDLEWARE = [
@@ -166,6 +167,23 @@ class Common(Configuration):
     CACHE_MIDDLEWARE_SECONDS = 100
     CACHE_MIDDLEWARE_KEY_PREFIX = ''
     SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
+    USE_S3 = values.BooleanValue(True)
+
+    if USE_S3:
+        AWS_DEFAULT_ACL = None
+        AWS_ACCESS_KEY_ID = values.SecretValue()
+        AWS_SECRET_ACCESS_KEY = values.SecretValue()
+        AWS_STORAGE_BUCKET_NAME = values.SecretValue()
+        AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+        AWS_S3_OBJECT_PARAMETERS = {
+            'CacheControl': 'max-age=86400',
+        }
+        AWS_LOCATION = 'static'
+
+        STATIC_URL = 'https://%s/%s/' % (
+            AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+        STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 class Development(Common):
